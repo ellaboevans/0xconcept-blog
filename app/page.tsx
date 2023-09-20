38f3fabcac9f8000fc4e8f3a5279c888e9b636a7
Evans <ellaboevans@gmail.com>
2023-09-20 15:03:56 +0000

    Connected Backend to Client

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
 import Hero from '@/components/Hero'
 
-export default function Home() {
+const getPosts = async () => {
+  const res = await fetch('http://localhost:3500/api/v1/posts', {
+    cache: 'no-store'
+  })
+  const posts = await res.json()
+  return posts
+}
+
+type dataProps = {
+  _id: string
+  title: string
+  summary: string
+  content: string
+  slug: string
+  createdAt: string
+}[]
+
+export default async function Home() {
+  const output: dataProps = await getPosts()
   return (
-    <>
-      <Hero />
-    </>
+    <div className="flex flex-col items-center overflow-x-hidden">
+      <Hero data={output} />
+    </div>
   )
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,65 @@ import blogImage from '../images/blog_image.jpg'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Hero() {
+// const data = [
+//   {
+//     id: 1,
+//     title: 'UX Review Presentations',
+//     description:
+//       'The latest industry news, technologies, linguistic forums and resources',
+//     tags: ['Design', 'Research', 'Coding'],
+//     img: blogImage,
+//     slug: 'ux-review-presentations'
+//   },
+//   {
+//     id: 2,
+//     title: 'Linguistics and the Web',
+//     description:
+//       'The latest industry news, technologies, linguistic forums and resources',
+//     tags: ['Design', 'Research', 'Coding'],
+//     img: blogImage,
+//     slug: 'linguistics-and-the-web'
+//   },
+//   {
+//     id: 3,
+//     title: 'Linguistics and the Web',
+//     description:
+//       'The latest industry news, technologies, linguistic forums and resources',
+//     tags: ['Design', 'Research', 'Coding'],
+//     img: blogImage,
+//     slug: 'linguistics-and-the-web'
+//   },
+//   {
+//     id: 4,
+//     title: 'Linguistics and the Web',
+//     description:
+//       'The latest industry news, technologies, linguistic forums and resources',
+//     tags: ['Design', 'Research', 'Coding'],
+//     img: blogImage,
+//     slug: 'linguistics-and-the-web'
+//   },
+//   {
+//     id: 5,
+//     title: 'Linguistics and the Web',
+//     description:
+//       'The latest industry news, technologies, linguistic forums and resources',
+//     tags: ['Design', 'Research', 'Coding'],
+//     img: blogImage,
+//     slug: 'linguistics-and-the-web'
+//   }
+// ]
+type dataProps = {
+  _id: string
+  title: string
+  summary: string
+  content: string
+  slug: string
+  author: string
+  createdAt: string
+  tag: string
+}
+export default function Hero({ data }: any) {
+  console.log(data)
   const { isDark } = useThemeContext()
   return (
     <main
@@ -16,7 +74,7 @@ export default function Hero() {
       } transition-all duration-75 flex flex-col items-center justify-center padding-container`}
     >
       <div
-        className={`text-center space-y-4 mb-5 ${mochain.variable} font-mochain`}
+        className={`text-center space-y-4 mt-10 mb-5 ${mochain.variable} font-mochain`}
       >
         <p className="text-lg">The Writer | The Linguist | The Coder</p>
         <h1 className="text-5xl font-semibold">Writings from our team</h1>
@@ -28,84 +86,52 @@ export default function Hero() {
         </h5>
       </div>
       <article className="flex flex-col items-center w-[90dvw]  overflow-x-hidden">
-        {
-          <Link
-            href="https://google.com"
-            className="w-[995px h-[500px] mb-4 bg-slate-400 relative"
-          >
-            <Image
-              src={blogImage}
-              alt="Testing"
-              className="w-full h-full object-cover"
-            />
-            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-slate-950 to-transparent h-1/2 z-">
-              <div className="flex flex-col px-6 items-start justify-center mt-5 h-full  text-white">
-                <p
-                  className={`text-base font-bold mb-3 tracking-wider ${overusedGrotesk.variable} font-overusedGrotesk`}
-                >
-                  <span>Evans Elabo </span> &bull; <span>19 Sep 2023</span>
-                </p>
-                <h1
-                  className={`text-4xl font-semibold ${mochain.variable} font-mochain`}
-                >
-                  UX Review Presentations
-                </h1>
-                <h5
-                  className={` text-xl ${overusedGrotesk.variable} font-overusedGrotesk`}
-                >
-                  The latest industry news, technologies, linguistic forums and
-                  resources
-                </h5>
-                <div className="mt-3 flex items-center gap-5 ">
-                  <p className="tag">Design</p>
-                  <p className="tag">Research</p>
-                  <p className="tag">Coding</p>
-                </div>
-              </div>
-            </div>
-          </Link>
-        }
         <div className="grid grid-cols-3 mb-10 gap-5 place-items-center mt-6">
-          {[1, 2, 3, 4, 5, 6].map((card, index) => (
-            <Link href="https://localhost" className="w-[350px]" key={index}>
-              <Image
-                src={blogImage}
-                alt="Test"
-                className="w-full h-60 object-cover"
-              />
-              <div className="p-3">
-                <p
-                  className={`text-base flex items-center space-x-2 font-bold mb-3 tracking-wider ${overusedGrotesk.variable} font-overusedGrotesk`}
-                >
-                  <span>Evans Elabo </span>
-                  <span> &bull; </span>
-                  <span>19 Sep 2023</span>
-                </p>
-                <div className="flex items-center justify-between">
-                  <h1
-                    className={`text-2xl font-semibold ${mochain.variable} font-mochain`}
+          {data &&
+            data.map((item: dataProps) => (
+              <Link href={`/${item.slug}`} className="w-[350px]" key={item._id}>
+                <Image
+                  src={blogImage}
+                  alt="Test"
+                  className="w-full h-60 object-cover"
+                />
+                <div className="p-3">
+                  <p
+                    className={`text-base flex items-center space-x-2 font-bold mb-3 tracking-wider ${overusedGrotesk.variable} font-overusedGrotesk`}
                   >
-                    UX Review Presentations
-                  </h1>
-                  <HiLink />
-                </div>
-                <h5
-                  className={` text-lg ${overusedGrotesk.variable} pr-[0.2rem] font-overusedGrotesk`}
-                >
-                  The latest industry news, technologies, linguistic forums and
-                  resources
-                </h5>
-                <div className="mt-3 flex items-center gap-5 ">
-                  <p className={`tag ${isDark ? '' : 'card-tag-dark'}`}>
-                    Design
+                    <span>{item.author}</span>
+                    <span> &bull; </span>
+                    <span>
+                      {new Date(item.createdAt).toString().slice(4, 15)}
+                    </span>
                   </p>
-                  <p className={`tag ${isDark ? '' : 'card-tag-dark'}`}>
-                    Research
-                  </p>
+                  <div className="flex items-center justify-between">
+                    <h1
+                      className={`text-2xl font-semibold ${mochain.variable} font-mochain`}
+                    >
+                      {item.title}
+                    </h1>
+                    <HiLink />
+                  </div>
+                  <h5
+                    className={` text-lg ${overusedGrotesk.variable} pr-[0.2rem] font-overusedGrotesk`}
+                  >
+                    {item.summary}
+                  </h5>
+                  <div className="mt-3 flex items-center gap-5 ">
+                    {item.tag ? (
+                      <p className={`tag ${isDark ? '' : 'card-tag-dark'}`}>
+                        {item.tag}
+                      </p>
+                    ) : (
+                      <p className={`tag ${isDark ? '' : 'card-tag-dark'}`}>
+                        Test Tag
+                      </p>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            ))}
         </div>
       </article>
     </main>
