5b8f8bd9532498c878b324e8e8e45b13e91818da
Evans <ellaboevans@gmail.com>
2023-09-21 11:32:45 +0000

    fix(app): Footer text font size
    Reduced the font size for good looks

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,18 +5,18 @@ import { overusedGrotesk } from '@/utils/Fonts'
 export default function Footer() {
   return (
     <footer
-      className={` py-5 flex flex-col items-center justify-center border-t ${overusedGrotesk.variable} font-overusedGrotesk text-lg max-w-[80dvw] md:max-w-[70dvw] mx-auto`}
+      className={` py-5 flex flex-col items-center justify-center border-t ${overusedGrotesk.variable} font-overusedGrotesk text-base max-w-[80dvw] md:max-w-[70dvw] mx-auto`}
     >
       <h1 className="text-center ">
-        Made with <span className="text-xl">&#9829;</span> by
+        Made with <span>&#9829;</span> by
         <span className="ml-2">
-          <a href="https://github.com/ellaboevans" className="underline">
+          <a href="https://linkedin.com/in/eelabo" className="underline">
             Evans Elabo
           </a>
           - Oxconcept
         </span>
       </h1>
-      <p>&copy;{year()} All Rights Reserved.</p>
+      <p>&copy; {year()} All Rights Reserved.</p>
     </footer>
   )
 }
