0033bb898645f1db91a2d6402ac9a8bcb5882d35
Evans <ellaboevans@gmail.com>
2023-09-22 13:58:54 +0000

    feat(app): Added Toaster Module
    Configured Toaster component at the High Level

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import { Toaster } from 'react-hot-toast'
 
 export const metadata: Metadata = {
   title: '0xConcept | Blog',
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-[rgb(36,36,36)] text-white">
+        <Toaster position="bottom-right" toastOptions={{ duration: 3000 }} />
         <Navbar />
         {children}
         <Footer />
