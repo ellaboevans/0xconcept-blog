4c844282d18cfd27542e9dda4a82d74b4f8a2465
Evans <ellaboevans@gmail.com>
2023-09-23 04:04:30 +0000

    Added username feature to display on login

--- app/dashboards/page.tsx
+++ app/dashboards/page.tsx
@@ -1,19 +1,25 @@
+'use client'
 import React from 'react'
 import { mochain } from '@/utils/Fonts'
 import CreatePost from '@/components/CreatePost'
 
+import useAuthContext from '@/hooks/useAuthContext'
+
 type Props = {}
 
 function Dashboard({}: Props) {
+  const { username } = useAuthContext()
+
   return (
     <main className="max-w-[80dvw] md:max-w-[70dvw] mx-auto my-10 flex flex-col items-center">
-      <h4 className={`${mochain.variable} font-mochain text-2xl mb-4`}>
-        Welcome, Evans &#128075;
+      <h4
+        className={`${mochain.variable} capitalize font-mochain text-2xl mb-4`}
+      >
+        Hi, {username ? username : 'Writer'} &#128075;
       </h4>
       <h1 className={`${mochain.variable} font-mochain text-4xl font-bold`}>
-        Create New Posts
+        Publish New Posts
       </h1>
-      {/* Create New Post Fields */}
       <CreatePost />
     </main>
   )
