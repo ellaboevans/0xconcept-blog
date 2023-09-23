f2175f90320c7c06c6b4e074177a850e2596217b
Evans <ellaboevans@gmail.com>
2023-09-23 04:07:24 +0000

    Configured Context for authentication

---
+++ contexts/AuthContextProvider.tsx
@@ -0,0 +1,41 @@
+'use client'
+import React, { createContext, useEffect, useState } from 'react'
+
+type Props = {
+  children: React.ReactNode
+}
+type Auth = string
+type AuthContextProps = {
+  username: Auth
+  setUsername: React.Dispatch<React.SetStateAction<string>>
+}
+
+export const AuthContext = createContext<AuthContextProps | null>(null)
+
+export default function AuthContextProvider({ children }: Props) {
+  const [username, setUsername] = useState<Auth>('')
+
+  useEffect(() => {
+    const getUsername = async () => {
+      const response = await fetch(
+        'http://localhost:3500/api/v1/auth/profile',
+        {
+          credentials: 'include'
+        }
+      )
+      const userData = await response.json()
+      setUsername(userData.username)
+    }
+    getUsername()
+  }, [])
+  return (
+    <AuthContext.Provider
+      value={{
+        username,
+        setUsername
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
