c4c0d28e6eb2c270fb0a5065a82c0bfd6e33c27a
Evans <ellaboevans@gmail.com>
2023-09-23 04:06:23 +0000

    Added logout/Login buttons and useAuthContext hook

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,8 +4,29 @@ import Link from 'next/link'
 import { mochain } from '@/utils/Fonts'
 import { motion } from 'framer-motion'
 import { SocialIcon } from 'react-social-icons'
+import toast from 'react-hot-toast'
+import useAuthContext from '@/hooks/useAuthContext'
+import { useRouter } from 'next/navigation'
 
 export default function Navbar() {
+  const { username } = useAuthContext()
+
+  const router = useRouter()
+
+  const handleLogout = async () => {
+    const response = await fetch('http://localhost:3500/api/v1/auth/logout', {
+      method: 'POST',
+      credentials: 'include'
+    })
+
+    if (response.status === 200) {
+      toast.success('Logout successful')
+      router.replace('/')
+    } else {
+      toast.error('Logout failed')
+    }
+  }
+
   return (
     <nav
       className={`flex items-center mx-auto justify-between bg-transparent transition-all duration-75 p-5 sticky top-0 backdrop-blur max-w-[80dvw] md:max-w-[70dvw] z-10`}
@@ -46,13 +67,24 @@ export default function Navbar() {
         transition={{
           duration: 1
         }}
-        className="flex items-center gap-5"
+        className="flex items-center"
       >
         <SocialIcon
           url="https://github.com/ellaboevans"
           bgColor="transparent"
           fgColor="white"
         />
+        <>
+          {username ? (
+            <button onClick={handleLogout} className="underline">
+              Logout
+            </button>
+          ) : (
+            <Link href="/auth/login" className="underline">
+              Login
+            </Link>
+          )}
+        </>
       </motion.div>
     </nav>
   )
