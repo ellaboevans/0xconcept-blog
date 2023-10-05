82b080a69227dc07dce43f1b3aebb1bb3b042586
Evans <ellaboevans@gmail.com>
2023-10-05 03:33:49 +0000

    refactor(app): Cleaned up code

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Hero from '@/components/Hero'
+import { dataProps } from '@/types/types'
 
-export default function PostsPage() {
+const getPosts = async () => {
+  const res = await fetch('https://ox-blog-api.onrender.com/api/v1/posts', {
+    cache: 'no-store'
+  })
+  const posts = await res.json()
+  return posts
+}
+
+export default async function Post() {
+  const output: dataProps = await getPosts()
   return (
-    <div className={`transition-all duration-75 h-screen padding-container`}>
-      <h1>Posts Page</h1>
+    <div>
+      <Hero data={output} />
     </div>
   )
 }
