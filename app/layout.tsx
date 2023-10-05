9c51d58f814adbdbb8457a6a84717b919178e925
Evans <ellaboevans@gmail.com>
2023-10-05 03:44:43 +0000

    Added Description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,9 @@ import { Toaster } from 'react-hot-toast'
 import AuthContextProvider from '@/contexts/AuthContextProvider'
 
 export const metadata: Metadata = {
-  title: '0xConcept | Blog',
-  description: 'A blog website for all'
+  title: '0xConcept | Evans Elabo',
+  description:
+    'Aspiring Linguistics graduate at Kwame Nkrumah University of Science and Technology with a strong passion for Computational Linguistics and a frontend engineer at SligthlyTechie with experience in building and designing web applications with ReactJS, NextJS, TailwindCSS, HTML, CSS, JavaScript, ExpressJS, NodeJS, MongoDB, SanityCMS and other web technologies.'
 }
 
 export default function RootLayout({
