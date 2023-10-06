ad984df23cca831b0cd4fce808cab2c1df77c98d
Evans <ellaboevans@gmail.com>
2023-10-06 21:15:51 +0000

    Removed Code

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,31 +5,8 @@ import { mochain } from '@/utils/Fonts'
 import { motion } from 'framer-motion'
 import { SocialIcon } from 'react-social-icons'
 import toast from 'react-hot-toast'
-import useAuthContext from '@/hooks/useAuthContext'
-import { useRouter } from 'next/navigation'
 
 export default function Navbar() {
-  const { username } = useAuthContext()
-
-  const router = useRouter()
-
-  // const handleLogout = async () => {
-  //   const response = await fetch(
-  //     'https://ox-blog-api.onrender.com/api/v1/auth/logout',
-  //     {
-  //       method: 'POST',
-  //       credentials: 'include'
-  //     }
-  //   )
-
-  //   if (response.status === 200) {
-  //     toast.success('Logout successful')
-  //     router.push('/')
-  //   } else {
-  //     toast.error('Logout failed')
-  //   }
-  // }
-
   return (
     <nav
       className={`flex items-center mx-auto justify-between bg-transparent transition-all duration-75 p-5 sticky top-0 backdrop-blur max-w-[100dvw] md:max-w-[70dvw] z-10`}
@@ -77,17 +54,6 @@ export default function Navbar() {
           bgColor="transparent"
           fgColor="white"
         />
-        <>
-          {/* {username ? (
-            <button onClick={handleLogout} className="underline">
-              Logout
-            </button>
-          ) : (
-            <Link href="/auth/login" className="underline">
-              Login
-            </Link>
-          )} */}
-        </>
       </motion.div>
     </nav>
   )
