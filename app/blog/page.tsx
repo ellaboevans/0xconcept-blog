0f8b3878e88163a658bd68530955777fe22d70d5
Evans <ellaboevans@gmail.com>
2023-10-06 04:47:11 +0000

    Added Metadata

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,12 @@
-import BlogPost from '@/components/BlogPost'
+import { Metadata } from 'next'
 import { dataProps } from '@/types/types'
+import BlogPost from '@/components/BlogPost'
+
+export const metadata: Metadata = {
+  title: '0xConcept | Blog',
+  description:
+    'A blog about my journey as a Software Engineer and my experiences as a Linguistics student as well other kinds of stories to authored by myself and many others.'
+}
 
 const getPosts = async () => {
   const res = await fetch('https://ox-blog-api.onrender.com/api/v1/posts', {
