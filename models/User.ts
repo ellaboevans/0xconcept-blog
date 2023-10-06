2a10bf745b35f923af6a442a66dffd7a49ae2bf4
Evans <ellaboevans@gmail.com>
2023-10-06 21:11:42 +0000

    Setting up database models

---
+++ models/User.ts
@@ -0,0 +1,27 @@
+import { Schema, models, model } from 'mongoose'
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 8
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const User = models.User || model('User', userSchema)
+module.exports = User
