a0b489165b08c6d33791b436a3356a1c57b7be6c
Evans <ellaboevans@gmail.com>
2023-10-06 04:51:26 +0000

    feat(app): Added openGraphs

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,6 +3,35 @@ import { ParamsProps } from '@/types/types'
 import GetSinglePost from '@/components/GetSinglePost'
 import { slugProps } from '@/types/types'
 
+import type { Metadata, ResolvingMetadata } from 'next'
+
+type Props = {
+  params: { slug: string }
+}
+
+export async function generateMetadata({
+  params
+}: ParamsProps): // parent: ResolvingMetadata
+Promise<Metadata> {
+  // read route params
+  const { slug } = params
+
+  // fetch data
+  const post = await fetch(
+    `https://ox-blog-api.onrender.com/api/v1/posts/${slug}`
+  ).then((res) => res.json())
+
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || []
+  return {
+    title: `0xConcept | ${post.title}`,
+    description: post.description,
+    openGraph: {
+      images: [`/${post.image}`]
+    }
+  }
+}
+
 const getSinglePost = async (slug: slugProps) => {
   const post = await fetch(
     `https://ox-blog-api.onrender.com/api/v1/posts/${slug}`,
