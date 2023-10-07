85c2175007a77a52a811471e6122052e38d967ec
Evans <ellaboevans@gmail.com>
2023-10-07 00:23:37 +0000

    Fixing bugs

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -6,6 +6,7 @@ export const GET = async (request: any) => {
   try {
     connect()
     const posts = await Post.find()
+    console.log(posts)
     return new NextResponse(JSON.stringify(posts), { status: 200 })
   } catch (error) {
     return new NextResponse('Database Error', { status: 500 })
