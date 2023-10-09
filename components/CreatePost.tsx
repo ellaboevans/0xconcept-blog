f23fa41e9dd239f23c4d321a45497d443349ba35
Evans <ellaboevans@gmail.com>
2023-10-09 20:01:24 +0000

    fix(app): Added vercel url to hit endpoints

--- components/CreatePost.tsx
+++ components/CreatePost.tsx
@@ -42,7 +42,7 @@ function CreatePost({ mutating }: { mutating: any }) {
     }
     console.log(postBody)
 
-    const response = await fetch('http://localhost:3000/api/posts', {
+    const response = await fetch('https://oxconcept.vercel.app/api/posts', {
       method: 'POST',
       body: JSON.stringify(postBody)
     })
