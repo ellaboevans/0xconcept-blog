1b3e0f9651d716e18018509d9e2003477d62e84d
Evans <ellaboevans@gmail.com>
2023-10-09 18:17:36 +0000

    Added Feteh Admin Post by email

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import { useSession } from 'next-auth/react'
 import image from '@/images/blog_image.jpg'
 import Image from 'next/image'
 import { MdDeleteForever } from 'react-icons/md'
-
+import useSWR from 'swr'
 type Props = {}
 
 function Dashboard({}: Props) {
@@ -15,46 +15,64 @@ function Dashboard({}: Props) {
 
   const router = useRouter()
 
+  const fetcher = (...args: any[]) => fetch(...args).then((res) => res.json())
+
+  const { data, mutate, error, isLoading } = useSWR(
+    `http://localhost:3000/api/posts?email=${session?.data?.user?.email}`,
+    fetcher
+  )
+
+  console.log(data)
+
   if (session.status === 'loading') return <p>Loading...</p>
 
   if (session.status === 'unauthenticated') {
-    router.replace('/dashboard/login')
+    router?.push('/dashboard/login')
   }
 
   console.log(session)
 
   if (session.status === 'authenticated') {
     return (
-      <main className="max-w-[100dvw] md:max-w-[70dvw] mx-auto my-10 px-4 md:gap-6 flex flex-col-reverse md:flex-row justify-between">
+      <main className="w-[100dvw] md:w-[70dvw] mx-auto my-10 px-4 md:gap-6 flex flex-col-reverse md:flex-row justify-between">
         <div className="max-w-2xl md:max-w-[22rem] mx-auto md:mx-0 max-h-60 md:max-h-[30rem] overflow-y-auto">
           {/* Design blog post card for here */}
-          {[1, 2, 3, 4, 5].map((item, i) => (
-            <div className="flex flex-col my-3" key={i}>
-              <div className="h-[12rem] w-full bg-gray-100 relative">
-                <Image
-                  src={image}
-                  alt="blog image"
-                  layout="fill"
-                  objectFit="cover"
-                />
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : data.length === 0 ? (
+            <p>There is no posts for {session?.data?.user?.email}</p>
+          ) : (
+            data?.map((post: any) => (
+              <div
+                className="flex flex-col my-3 w-[20rem] pr-2 "
+                key={post._id}
+              >
+                <div className="h-[12rem] bg-gray-100 relative">
+                  <Image
+                    src={image}
+                    alt="blog image"
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+                <div className="flex text-xl items-center justify-between gap-4 mt-3 ">
+                  <h1
+                    className={` font-semibold ${overusedGrotesk.variable} font-overusedGrotesk`}
+                  >
+                    {post.title}
+                  </h1>
+                  <span
+                    className="text-3xl cursor-pointer"
+                    onClick={() => console.log('post deleted!')}
+                  >
+                    <MdDeleteForever />
+                  </span>
+                </div>
               </div>
-              <div className="flex text-xl items-center justify-between gap-4 mt-3">
-                <h1
-                  className={` font-semibold ${overusedGrotesk.variable} font-overusedGrotesk`}
-                >
-                  Publish New Post &rarr; wai na we have missed you wai
-                </h1>
-                <span
-                  className="text-3xl cursor-pointer"
-                  onClick={() => console.log('post deleted!')}
-                >
-                  <MdDeleteForever />
-                </span>
-              </div>
-            </div>
-          ))}
+            ))
+          )}
         </div>
-        <CreatePost />
+        <CreatePost mutating={mutate} />
       </main>
     )
   }
